cd1ad2bc64efb9a5fc911ac4fa9fd1609f43d753
@manbek1978 <worldismineok@gmail.com>
2021-04-29 20:37:00 +0500

    SpringBootJava
    Create website with javaSpringBoot

---
+++ TestControl/src/main/java/com/springBoot/TestControl/TestControlApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.TestControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestControlApplication.class, args);
+	}
+
+}
---
+++ TestControl/src/main/java/com/springBoot/TestControl/User.java
@@ -0,0 +1,99 @@
+package com.springBoot.TestControl;
+
+import java.sql.Date;
+
+public class User {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String name;
+    private String email;
+    private String password;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String gender;
+    private String note;
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    private String profession;
+    private boolean married;
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    private Date birthday;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", note='" + note + '\'' +
+                ", profession='" + profession + '\'' +
+                ", married=" + married +
+                ", birthday=" + birthday +
+                '}';
+    }
+    // getters
+    // setters
+
+    // override toString()
+}
